47d2ee1b34f788eaab0c9a1ad63eb0b1c0009ef1
Rushikesh Badgujar <82447941+rishi7822@users.noreply.github.com>
2022-11-22 00:57:22 +0530

    Add files via upload

---
+++ smallestelelists.py
@@ -0,0 +1,8 @@
+list1 = ['piyush','deep','rushikesh']
+
+print("smallest element in list1 is :",max(list1))
+
+####
+#largest elemnt in list
+list2 = [245,122,454,5656,8,88888]
+print("largest element in list2 is:",max(list2))
